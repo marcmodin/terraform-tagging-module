6d3216ffc148dbc8cbe3db41ca04ea907a6edcca
marc <marcmodin@yahoo.com>
2024-11-10 15:35:21 +0100

    ci: fix pre-commit errors

--- examples/s3_nested_module/s3/variables.tf
+++ examples/s3_nested_module/s3/variables.tf
@@ -1,8 +1,10 @@
 variable "context" {
+  description = "The context to use for the tags"
   type = any
 }
 
 variable "tags" {
+  description = "The tags to apply to the resources"
   type    = any
   default = {}
 }
