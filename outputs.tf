121988f361371fda5106e2c01291d7e22686eed3
marc <marcmodin@yahoo.com>
2024-11-10 15:15:51 +0100

    feat: refactor codebase and release new version
    BREAKING-CHANGE: adding context functionality requires new variables and what values are outputted

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,28 @@
-output "tags" {
-  description = "The tags map to be applied to resources"
-  value       = local.base_tags
-}
+####################################################################################################
+# Outputs
+####################################################################################################
 
-output "merged_tags" {
-  description = "The merged tags map to be applied to resources"
+output "tags" {
+  description = "The combined tags to be applied to resources"
   value       = local.merged_tags
 }
 
 output "resource_id" {
-  description = "The generated resource ID based on environment and project ID"
-  value       = local.id
+  description = "The generated resource ID based on the specified format"
+  value       = local.resource_id
 }
 
-output "resource_id_with_hash" {
-  description = "The generated resource ID with hash based on environment and project ID"
-  value       = local.hashed_id
-}
-
-output "s3_resource_id" {
-  description = "The generated resource ID for S3 based on environment and project ID"
-  value       = local.s3_safe_id
+output "resource_ids" {
+  description = "A map of resource IDs including the base ID and service-specific IDs"
+  value       = local.resource_id_map
 }
 
 output "additional_tags" {
   description = "The additional tags to be merged with the generated tags"
   value       = var.additional_tags
+}
+
+output "context" {
+  description = "The context of the resource. Useful for troubleshooting and debugging"
+  value       = var.context
 }
